a2de8c4695fa3926c3da78fffb98a9bf11876729
Alin Dicu <alinddicu@yahoo.com>
2019-01-24 21:51:55 +0100

    board needs to be observable for reinit

--- Dont.Stop.Coding/Dont.Stop.Coding/src/scripts/ViewModel/GameOfLifeViewModel.ts
+++ Dont.Stop.Coding/Dont.Stop.Coding/src/scripts/ViewModel/GameOfLifeViewModel.ts
@@ -5,7 +5,7 @@ namespace ViewModel {
 		private params: GoL.Drawing.ParamsForm;
 
 		public pageName = "game-of-life";
-		public board: GoL.Drawing.Board;
+		public board: KnockoutObservable<GoL.Drawing.Board> = ko.observable(null);
 		public isParamsVisible: KnockoutObservable<boolean> = ko.observable(false);
 		public isExportVisible: KnockoutObservable<boolean> = ko.observable(false);
 		public isImportVisible: KnockoutObservable<boolean> = ko.observable(false);
@@ -13,7 +13,7 @@ namespace ViewModel {
 		constructor(workflow: IWorkflow) {
 			this.workflow = workflow;
 			this.params = new GoL.Drawing.ParamsForm();
-			this.board = this.params.init();
+			this.board(this.params.init());
 		}
 
 		public render(): void {
@@ -21,42 +21,42 @@ namespace ViewModel {
 		}
 
 		public reinitBoard(): void {
-			this.board = this.params.init();
-			this.board.initCellButtonsInSquare(this.params.options);
+			this.board(this.params.init());
+			this.board().initCellButtonsInSquare(this.params.options);
 			this.isParamsVisible(false);
-			this.board.isEnabled(true);
+			this.board().isEnabled(true);
 		}
 
 		public showParams(): void {
 			const currentValue = this.isParamsVisible();
 			this.isParamsVisible(!currentValue);
-			this.board.isEnabled(currentValue);
+			this.board().isEnabled(currentValue);
 		}
 
 		public exportAliveCells(): void {
 			const currentValue = this.isExportVisible();
-			this.board.isEnabled(currentValue);
+			this.board().isEnabled(currentValue);
 			this.isExportVisible(!currentValue);
 			var textArea = document.getElementById("exportTextArea") as HTMLTextAreaElement;
 			if (textArea) {
-				textArea.value = this.board.export();
+				textArea.value = this.board().export();
 			}
 		}
 
 		public showImportAliveCells(): void {
 			const currentValue = this.isImportVisible();
 			this.isImportVisible(!currentValue);
-			this.board.isEnabled(currentValue);
+			this.board().isEnabled(currentValue);
 		}
 
 		public importAliveCells(): void {
 			var textArea = document.getElementById("importTextArea") as HTMLTextAreaElement;
 			if (textArea && textArea.value) {
-				this.board.import(textArea.value);
+				this.board().import(textArea.value);
 			}
 
 			this.isImportVisible(false);
-			this.board.isEnabled(true);
+			this.board().isEnabled(true);
 		}
 	}
 }
