3693267b9561e6ec92ff3f71590953784aa9e196
Alin Dicu <alinddicu@yahoo.com>
2019-01-26 00:18:51 +0100

    Revert "added finch, maybe i can use it ..."
    This reverts commit a34421e532e1043d6a7ffa08283b01e636307d19.

--- Dont.Stop.Coding/Dont.Stop.Coding/gulpfile.js
+++ Dont.Stop.Coding/Dont.Stop.Coding/gulpfile.js
@@ -10,8 +10,7 @@ var paths = {
 		'node_modules/linq/linq.min.js',
 		'node_modules/knockout/build/output/knockout-latest.js',
 		'node_modules/sammy/lib/min/sammy-latest.min.js',
-		'node_modules/jquery/dist/jquery.min.js',
-		'node_modules/finch/bin/finch.js'
+		'node_modules/jquery/dist/jquery.min.js'
 	],
 	transpiled: ['src/scripts/**/*.js'],
 	typescript: ['src/scripts/**/*.ts'],
