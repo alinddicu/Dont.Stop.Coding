4a51a907c4a55afc62024c434bd928077ce1c208
Alin Dicu <alinddicu@yahoo.com>
2019-01-26 10:14:39 +0100

    removed duplication into Tools.InputValueConverter

---
+++ Dont.Stop.Coding/Dont.Stop.Coding/src/scripts/Tools/InputValueConverter.ts
@@ -0,0 +1,26 @@
+﻿namespace Tools {
+	export class InputValueConverter {
+		public static valueOrDefault<T>(elementId: string, defaultValue: any): T {
+
+			let value;
+			try {
+				const inputField = document.getElementById(elementId) as HTMLFormElement;
+				if (typeof defaultValue === "number") {
+					value = inputField.valueAsNumber;
+				}
+				else if (typeof defaultValue === "string") {
+					value = inputField.value;
+				}
+				else if (typeof defaultValue === "boolean") {
+					value = inputField.checked;
+				} else {
+					value = inputField.valueAsDate;
+				}
+			} catch (e) {
+				console.warn(`Problem parsing '${elementId}'`);
+			}
+
+			return value ? value : defaultValue;
+		}
+	}
+}
