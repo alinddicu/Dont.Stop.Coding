1673798a0eb6d860289d769c0fa131a93178dafa
Alin Dicu <alinddicu@yahoo.com>
2019-01-28 21:46:23 +0100

    better with 7 cells grid

--- Dont.Stop.Coding/Dont.Stop.Coding/src/scripts/GoL/Drawing/ParamsForm.ts
+++ Dont.Stop.Coding/Dont.Stop.Coding/src/scripts/GoL/Drawing/ParamsForm.ts
@@ -4,9 +4,9 @@
 
 		public init(): Board {
 			this.options = {
-				cellsPerRow: Tools.InputValueConverter.valueOrDefault<number>("cellsPerRow", 6),
+				cellsPerRow: Tools.InputValueConverter.valueOrDefault<number>("cellsPerRow", 7),
 				isShowCellsCoordinates: Tools.InputValueConverter.valueOrDefault<boolean>("isShowCellsCoordinates", false),
-				cellSize: Tools.InputValueConverter.valueOrDefault<number>("cellSize", 50),
+				cellSize: Tools.InputValueConverter.valueOrDefault<number>("cellSize", 42),
 				normalMutationDelay: Tools.InputValueConverter.valueOrDefault<number>("normalMutationDelay", 100),
 				rapidMutationDelay: Tools.InputValueConverter.valueOrDefault<number>("rapidMutationDelay", 10),
 				aliveCellColor: Tools.InputValueConverter.valueOrDefault<string>("aliveCellColor", "#374785"),
