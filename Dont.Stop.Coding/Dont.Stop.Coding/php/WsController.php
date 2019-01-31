fb9b0a44750c0cf0c9d6672583e4fe8a90a5f54a
Alin Dicu <alinddicu@yahoo.com>
2019-01-31 23:00:15 +0100

    fixed xml -> json

--- Dont.Stop.Coding/Dont.Stop.Coding/php/WsController.php
+++ Dont.Stop.Coding/Dont.Stop.Coding/php/WsController.php
@@ -7,7 +7,8 @@ class WsController extends AppController
     public function index()
     {
 		$url = "http://www.cbn.com/cbnnews/world/feed/";
-		return $this->response->withType("application/json")->withStringBody(file_get_contents($url));	
-        //$this->set(compact('articles'));
+		$fileContents = file_get_contents($url);
+		$json = json_encode(simplexml_load_string($fileContents, 'SimpleXMLElement', LIBXML_NOCDATA));
+		return $this->response->withType("application/json")->withStringBody($json);	
     }
 }
