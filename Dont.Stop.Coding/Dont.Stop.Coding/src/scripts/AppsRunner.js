977dfa3fce4c9b2dcb8a06381213de954f166672
Alin Dicu <alinddicu@yahoo.com>
2019-02-01 23:51:45 +0100

    fix usinf testing api

--- Dont.Stop.Coding/Dont.Stop.Coding/src/scripts/AppsRunner.js
+++ Dont.Stop.Coding/Dont.Stop.Coding/src/scripts/AppsRunner.js
@@ -7,8 +7,8 @@ function AppsRunner() {
 	var self = this;
 	self.currentViewModel = ko.observable();
 	self.menuOpen = ko.observable(false);
-	self.api = new ApiTest();
-	//self.api = new Api($);
+	//self.api = new ApiTest();
+	self.api = new Api($);
 
 	function closeMenu() {
 		self.menuOpen(false);
