f7a3e72471e4ed737cae18001efbd8e9bae32405
Alin Dicu <alinddicu@yahoo.com>
2019-02-01 23:46:35 +0100

    added more rss feeds and athe menu

--- Dont.Stop.Coding/Dont.Stop.Coding/src/scripts/ViewModel/RssAggregatorViewModel.ts
+++ Dont.Stop.Coding/Dont.Stop.Coding/src/scripts/ViewModel/RssAggregatorViewModel.ts
@@ -3,20 +3,36 @@
 namespace ViewModel {
 	export class RssAggregatorViewModel extends ViewModelBase {
 		public pageName = "rss-aggregator";
-		public rssItems: KnockoutObservableArray<string> = ko.observableArray([]);
+		public rssItems: KnockoutObservableArray<any> = ko.observableArray([]);
+		public menuOpen = ko.observable(false);
+		public rssFeeds: any = [
+			{
+				channel: "CBNNews.com",
+				url: "http://www.cbn.com/cbnnews/world/feed/"
+			},
+			{
+				channel: "BBC News - World",
+				url: "http://feeds.bbci.co.uk/news/world/rss.xml"
+			},
+			{
+				channel: "Reuters: World News",
+				url: "http://feeds.reuters.com/Reuters/worldNews"
+			}
+		];
 
 		constructor(workflow: IAppsRunner) {
 			super(workflow);
 			this.backgroundColor("#8fc1e3");
-			this.render();
+			this.callUrl("http://www.cbn.com/cbnnews/world/feed/");
 		}
 
-		public render(): void {
+		public callUrl(url: string): void {
 			var self = this;
 			super.render();
-			this.workflow.api.getRss("http://www.cbn.com/cbnnews/world/feed/")
+			this.workflow.api.getRss(url)
 				.done(function (rss: any) {
-					rss.channel.item.map(function(rssItem: any) {
+					self.rssItems([]);
+					rss.channel.item.map(function (rssItem: any) {
 						self.rssItems.push(rssItem);
 					});
 				})
