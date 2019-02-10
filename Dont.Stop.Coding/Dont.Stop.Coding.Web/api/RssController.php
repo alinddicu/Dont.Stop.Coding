8d261a4f1b05284a9a569b612b3e9078e96a773f
Alin Dicu <alinddicu@yahoo.com>
2019-02-10 14:14:21 +0100

    validated tests, going to prod!!!

--- Dont.Stop.Coding/Dont.Stop.Coding.Web/api/RssController.php
+++ Dont.Stop.Coding/Dont.Stop.Coding.Web/api/RssController.php
@@ -6,10 +6,21 @@ class RssController extends AppController
 {
     public function index()
     {
-		//$url = "http://www.cbn.com/cbnnews/world/feed/";
 		$feed = urldecode($_GET['feed']);
 		$fileContents = file_get_contents($feed);
-		$json = json_encode(simplexml_load_string($fileContents, 'SimpleXMLElement', LIBXML_NOCDATA));
+		$xml = simplexml_load_string($fileContents, 'SimpleXMLElement', LIBXML_NOCDATA);
+		
+		if (strpos($fileContents, 'media:thumbnail')) {
+			$namespaces = $xml->getNamespaces(true); // get namespaces
+
+			$items = array();
+			foreach ($xml->channel->item as $item) {
+			  $thumbnail_url = trim((string) $item->children($namespaces['media'])->thumbnail->attributes()->url);
+			  $item->thumbnail = $thumbnail_url;
+			}
+		}
+		
+		$json = json_encode($xml);
 		return $this->response->withType("application/json")->withStringBody($json);	
     }
 }
